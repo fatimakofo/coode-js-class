6b7e48caf65c1620dae22c105eb3ee4edce5308d
fatimakofo <fatima26884646@gmail.com>
2023-05-10 11:29:38 +0100

    changes

--- fatima's assignment/to-do-list/script.js
+++ fatima's assignment/to-do-list/script.js
@@ -3,61 +3,114 @@ let inputDate = document.getElementById("date");
 let save = document.getElementById("save");
 let form = document.querySelector("form");
 let listContainer = document.querySelector("ul");
+let task = document.getElementById("task");
+let close = document.getElementById("close");
 let deleted = document.getElementsByClassName("delete");
+let list = document.getElementsByClassName("lists");
+
+// const list = document.querySelector("li");
+
+console.log(list);
+// open and close button
+function add() {
+  task.style.display = "none";
+  close.style.display = "block";
+  form.style.display = "block";
+}
+
+function closing() {
+  close.style.display = "none";
+  task.style.display = "block";
+  form.style.display = "none";
+}
 
 // setting the values
 let listArr = [];
 let todoContent = {
-    todoText :"",
-    todoDate :"",
+  id: 0,
+  todoText: "",
+  todoDate: "",
 };
 
-inputText.addEventListener("change", function (event){
- todoContent.todoText = event.target.value;
- console.log(todoContent)
+inputText.addEventListener("change", function (event) {
+  todoContent.todoText = event.target.value;
+  console.log(todoContent);
 });
 
-inputDate.addEventListener("change", function (event){
-    todoContent.todoDate = event.target.value;
-    console.log(todoContent)
-   });
-  
+inputDate.addEventListener("change", function (event) {
+  todoContent.todoDate = event.target.value;
+  console.log(todoContent);
+});
 
-form.addEventListener("submit", function(event){
- event.preventDefault();
- listArr.push(todoContent);
+form.addEventListener("submit", function (event) {
+  todoContent.id = Math.floor(Math.random() * 110000);
+  event.preventDefault();
+  listArr.push(todoContent);
 
+  // resetting the values
+  // you reset the value so the new value wont override the previous values
+  todoContent = {
+    todoText: "",
+    todoDate: "",
+  };
+  inputText.value = "";
+  inputDate.value = "";
+  getList();
+});
 
-// resetting the values
-todoContent = {
-    todoText :"",
-    todoDate :"",
+function getList() {
+  let listContent = "";
+  listArr.forEach(function (value) {
+    listContent += `<li class = "lists">
+                        <p>
+                            ${value.todoText} <br/>
+                            <span>${value.todoDate}</span>
+                        <p/>
+                        <button class="delete" onclick="deleteTask(list)">Delete<button/>
+                    </li>`;
+  });
+  listContainer.innerHTML = listContent;
+}
+
+let deleteTask = (list) => {
+  if (confirm("Are you sure?")) {
+    //   listArr = listArr.filter((list) => {
+    //     return list.id != id;
+    //   });
+
+    listArr.splice(listArr.indexOf(list), 1);
+    getList();
+  }
 };
-inputText.value = "";
-inputDate.value = "";
 
-let listContent = "";
-listArr.forEach(function (value) {
-    listContent += `<li>
-                    <p>
-                    ${value.todoText} <br/>
-                    <span>${value.todoDate}</span>
-                    <p/>
-                    <button class="delete">Delete<button/>
-                    
-                </li>`;
-});
-listContainer.innerHTML = listContent;
+
+list.addEventListener("click", function(e){
+  if(e.target.tagname === "li"){
+    e.target.classList.toggle("color");
+  };
+  getList();
+ console.log(li);
 });
 
-// deleted.addEventListener("click", function(event){
-//      event.listContainer.removeChild(listContent);
+
+// listArr.forEach((listed) => {
+
+//   listed.addEventListener("dblclick", () => todoCompletion(list.id));
 // });
 
-// const deleteTodo= (todoList) => {
-//     const areYouSure = confirm("Are you sure?");
-//     if (areYouSure) {
-//       listArr = listArr.filter((todo) => todoContent != todoList);
-//       getAllTodo();
-//     }
-//   };
+getList();
+// todo completeion
+// const todoCompletion = (list) =>{
+//   if (confirm ("Are you sure?")){
+//     listArr.forEach((todo) => {
+      // if (list.id == list) {
+      //   todo.completed = !list.completed;
+      // }
+//       todo.style.background = "yellow";
+//     });
+//     getList();
+//   }
+// };
+
+// for the delete button
+/* <button class="delete" onclick="deleteTask('${value}')">Delete<button/> */
